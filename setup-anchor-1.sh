3fc476df520382ea7f42ec981731accbca9cd415
OfWolfAndMan <therootbridge@hotmail.com>
2017-06-04 09:46:30 -0400

    Fixed pf.conf filename

--- setup-anchor-1.sh
+++ setup-anchor-1.sh
@@ -3,7 +3,7 @@
 #Run this before running the script to update blocklists
 
 sudo echo "anchor "emerging-threats"
-load anchor "emerging-threats" from "/etc/pf.anchors/emerging-threats"" >> /etc/pf.conf.test
+load anchor "emerging-threats" from "/etc/pf.anchors/emerging-threats"" >> /etc/pf.conf
 
 sudo echo "table <emerging-threats> persist file "/etc/emerging-threats.txt"
 block log from <emerging-threats> to any" >> /etc/pf.anchors/emerging-threats
